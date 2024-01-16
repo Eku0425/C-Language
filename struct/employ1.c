949dfe1b0d460accb6dc17d91625c99a9c643da6
Ekta Trivedi <149374328+Eku0425@users.noreply.github.com>
2024-01-16 22:27:52 +0530

    Add files via upload

---
+++ struct/employ1.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct Employee
+{
+	int emp_id;	
+	char emp_name[50];	
+	int emp_age;	
+	char emp_role[50];	
+	char emp_city[50];	
+	int emp_experience;
+	char emp_company_name[50];		
+};
+
+void main()
+{
+	char a[50],b[50],c[50],d[50];
+	int n;//number of employees
+	printf("Enter the number of employees : ");
+	scanf("%d",&n);//user input
+	struct Employee employee[n];
+	int i;//loop counter
+	
+	for(i=0; i<n; i++)
+	{
+	    //user input of details of employees
+		printf("\nEmployee %d\n",i+1);
+		printf("Enter your ID : ");
+		scanf("%d",&employee[i].emp_id);
+		
+		printf("Enter your Name : ");
+		scanf(" %[^\n]s",&a);
+		strcpy(employee[i].emp_name,a);
+		
+		printf("Enter your age : ");
+		scanf("%d",&employee[i].emp_age);
+		
+		printf("Enter your role : ");
+		scanf(" %[^\n]s",&b);
+		strcpy(employee[i].emp_role,b);
+		
+		printf("Enter your city : ");
+		scanf(" %[^\n]s",&c);
+		strcpy(employee[i].emp_city,c);
+		
+		printf("Enter Your experience : ");
+		scanf("%d",&employee[i].emp_experience);
+		
+		printf("Enter Your company name : ");
+		scanf(" %[^\n]s",&d);
+		strcpy(employee[i].emp_company_name,d);
+		printf("\n");
+	}
+	for(i=0; i<n; i++)
+	{
+	    //print details of employees
+		printf("ID : %d\n",employee[i].emp_id);
+		printf("Name : %s\n",employee[i].emp_name);
+		printf("Age : %d\n",employee[i].emp_age);
+		printf("Role : %s\n",employee[i].emp_role);
+		printf("City : %s\n",employee[i].emp_city);
+		printf("Experience : %d\n",employee[i].emp_experience);
+		printf("Company Name : %s\n\n",employee[i].emp_company_name);
+	}
+}
+
+
---
+++ struct/student1_id.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct Student
+{
+	int stu_id;	
+	char stu_name[50];	
+	int stu_age;	
+	char stu_course[50];	
+	char stu_city[50];	
+	int stu_standard;
+	char stu_school[50];		
+};
+
+void main()
+{
+	char a[50],b[50],c[50],d[50];
+	struct Student student[3];
+	int i;//loop counter
+	
+	for(i=0; i<3; i++)
+	{
+	    //student details user input
+		printf("Student %d\n",i+1);
+		printf("Enter your ID : ");
+		scanf("%d",&student[i].stu_id);
+		
+		printf("Enter your Name : ");
+		scanf(" %[^\n]s",&a);
+		strcpy(student[i].stu_name,a);
+		
+		printf("Enter your age : ");
+		scanf("%d",&student[i].stu_age);
+		
+		printf("Enter your Course : ");
+		scanf(" %[^\n]s",&b);
+		strcpy(student[i].stu_course,b);
+		
+		printf("Enter your city : ");
+		scanf(" %[^\n]s",&c);
+		strcpy(student[i].stu_city,c);
+		
+		printf("Enter Your standard : ");
+		scanf("%d",&student[i].stu_standard);
+		
+		printf("Enter Your school : ");
+		scanf(" %[^\n]s",&d);
+		strcpy(student[i].stu_school,d);
+		printf("\n");
+	}
+	printf("\n");
+	for(i=0; i<3; i++)
+	{
+	    //print student details
+		printf("ID : %d\n",student[i].stu_id);
+		printf("Name : %s\n",student[i].stu_name);
+		printf("Age : %d\n",student[i].stu_age);
+		printf("Course : %s\n",student[i].stu_course);
+		printf("City : %s\n",student[i].stu_city);
+		printf("Standard : %d\n",student[i].stu_standard);
+		printf("School : %s\n\n",student[i].stu_school);
+	}
+}
+
+
